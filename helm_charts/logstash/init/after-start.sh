70a336f897a1981b0f8cbbe6350cb78505a56785
Jillian Rowe <jillian.e.rowe@gmail.com>
2020-04-01 20:24:56 +0300

    MAYBE IT WORKS

--- helm_charts/logstash/init/after-start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd /tmp
-wget http://www.dbschema.com/jdbc-drivers/MongoDbJdbcDriver.zip
-unzip MongoDbJdbcDriver.zip
-
-/usr/share/logstash/bin/logstash-plugin install logstash-output-mongodb
